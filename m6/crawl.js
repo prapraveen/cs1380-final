528b4fa3d85d6108b367b64d098306d68e161319
prapraveen <praveencubes@gmail.com>
2026-04-12 18:14:34 -0400

    deduplication

--- m6/crawl.js
+++ m6/crawl.js
@@ -19,7 +19,7 @@ groupA[id.getSID(n2)] = n2;
 groupA[id.getSID(n3)] = n3;
 
 // let seed_urls = ["apartments.com", "craigslist.org", "zillow.com", "airbnb.com"];
-let seed_urls = ["https://airbnb.com"];
+let seed_urls = ["https://wikipedia.org"];
 let seed_url_keys = seed_urls.map(encodeURIComponent);
 
 
@@ -44,111 +44,154 @@ distribution.local.groups.put({gid: "urls_queue", hash: id.naiveHash}, groupA, (
 
         insert_items(0, () => {
           const mapper = (encodedURL, url, cb) => {
-
-            const curlHTML = (url, redirectCounter, cb) => {
-              if (!url.endsWith('/')) {
-                url += '/';
-              }
-
-              if (redirectCounter <= 0) {
-                console.log("hit redirect limit");
-                return cb("")
-              }
-
-              if (!url.startsWith("http://") && !url.startsWith("https://")) {
-                url = "https://" + url;
-              }
-
-              let proto;
-              proto = url.startsWith("https") ? distribution.https : distribution.http;
-
-              proto.get(url, (res) => {
-                if (res.statusCode >= 300 && res.statusCode <= 399) {
-                  console.log("hit redirect...")
-                  const redirectURL = res.headers["location"];
-                  if (!redirectURL) {
-                    console.log("no redirect url");
-                    return cb("");
-                  }
-                  const new_url = new URL(redirectURL, url).href;
-                  console.log("redirecting...");
-                  return curlHTML(new_url, redirectCounter - 1, cb);
-                }
-
-                if (res.statusCode != 200) {
-                  console.log("invalid status code:", res.statusCode);
-                  return cb("");
-                }
-
-                if (!res.headers["content-type"]) {
-                  console.log("no content type");
-                  return cb("");
-                }
-
-                if (!res.headers["content-type"].includes("text/html")) {
-                  console.log("not text");
-                  return cb("");
-                }
-
-                let data = '';
-                res.on('data', (chunk) => {
-                  data += chunk;
-                });
-
-                res.on('end', () => {
-                  cb(data);
-                })
-
-                res.on('error', (e) => {
-                  console.log(e);
-                  cb("");
-                })
-              })
-            }
-
-            curlHTML(url, 3, (data) => {
-              distribution["page_content"].store.put(data, encodedURL, (e, v) => {
-                if (e) console.log("line 67:", e);
-                res = [];
-                const dom = new distribution.JSDOM(data);
-                const anchors = dom.window.document.querySelectorAll('a[href]');
-
-                for (const anchor of anchors) {
-                  const href = anchor.getAttribute('href');
-                  if (href.startsWith("#")) continue;
-
-                  let baseURL = url;
-                  if (baseURL.endsWith('index.html')) {
-                    baseURL = baseURL.slice(0, baseURL.length - 'index.html'.length);
-                  } else if (!baseURL.endsWith('/')) {
-                    baseURL += '/';
-                  }
-
-                  const new_url = new distribution.URL(href, baseURL).href;
-                  // res.push( {[encodeURIComponent(new_url)]: 1} );
-                  res.push(new_url);
-                }
-
+            distribution.page_content.store.keyExists(encodedURL, (e, exists) => {
+              if (exists) {
                 distribution.urls_queue.store.del(encodedURL, (e, v) => {
-                  if (e) console.log(e);
-                  let newURLsCounter = 0;
-                  let totalNewUrls = res.length
-                  if (newURLsCounter == totalNewUrls) {
-                    cb(res);
+                  return cb([]);
+                })
+              } else {
+                const curlHTML = (url, redirectCounter, cb) => {
+                  console.log("CRAWLING:", url);
+                  if (!url.endsWith('/')) {
+                    url += '/';
                   }
-                  res.forEach((u) => {
-                    distribution.urls_queue.store.put(u, encodeURIComponent(u), (e, v) => {
-                      if (e) console.log(e);
-                      newURLsCounter++;
-                      if (newURLsCounter == totalNewUrls) {
-                        res = res.map((u) => {return {[encodeURIComponent(u)]: 1}; });
-                        cb(res);
+
+                  if (redirectCounter <= 0) {
+                    console.log("hit redirect limit");
+                    return cb("")
+                  }
+
+                  if (!url.startsWith("http://") && !url.startsWith("https://")) {
+                    url = "https://" + url;
+                  }
+
+                  let proto;
+                  proto = url.startsWith("https") ? distribution.https : distribution.http;
+
+                  const options = {
+                    hostname: new URL(url).hostname,
+                    path: new URL(url).pathname + new URL(url).search,
+                    method: "GET",
+                    headers: {
+                      "User-Agent": "Cs1380-final-project/1.0 (praveen_prabaharan@brown.edu)"
+                    }
+                  }
+
+                  const req = proto.request(options, (res) => {
+                    if (res.statusCode >= 300 && res.statusCode <= 399) {
+                      console.log(url, "hit redirect...")
+                      const redirectURL = res.headers["location"];
+                      if (!redirectURL) {
+                        console.log(url, "no redirect url");
+                        return cb("");
                       }
+                      const new_url = new URL(redirectURL, url).href;
+                      console.log(url, "redirecting to", new_url);
+                      return curlHTML(new_url, redirectCounter - 1, cb);
+                    }
+
+                    if (res.statusCode != 200) {
+                      console.log(url, "invalid status code:", res.statusCode);
+                      return cb("");
+                    }
+
+                    if (!res.headers["content-type"]) {
+                      console.log(url, "no content type");
+                      return cb("");
+                    }
+
+                    if (!res.headers["content-type"].includes("text/html")) {
+                      console.log(url, "not text");
+                      return cb("");
+                    }
+
+                    let data = '';
+                    res.on('data', (chunk) => {
+                      data += chunk;
+                    });
+
+                    res.on('end', () => {
+                      cb(data);
+                    })
+
+                    res.on('error', (e) => {
+                      console.log(e);
+                      cb("");
                     })
                   })
+
+                  req.on('error', (e) => {
+                    console.log(e);
+                    cb("");
+                  })
+                  req.end();
+                }
+
+                curlHTML(url, 3, (data) => {
+                  if (data === "") {
+                    distribution.urls_queue.store.del(encodedURL, (e, v) => {
+                      return cb([]);
+                    })
+                  } else {
+                    distribution["page_content"].store.put(data, encodedURL, (e, v) => {
+                      if (e) console.log("line 67:", e);
+                      res = [];
+                      const dom = new distribution.JSDOM(data);
+                      const anchors = dom.window.document.querySelectorAll('a[href]');
+
+                      for (const anchor of anchors) {
+                        const href = anchor.getAttribute('href');
+                        if (href.startsWith("#")) continue;
+
+                        let baseURL = url;
+                        if (baseURL.endsWith('index.html')) {
+                          baseURL = baseURL.slice(0, baseURL.length - 'index.html'.length);
+                        } else if (!baseURL.endsWith('/')) {
+                          baseURL += '/';
+                        }
+
+                        const new_url = new distribution.URL(href, baseURL).href;
+                        // res.push( {[encodeURIComponent(new_url)]: 1} );
+                        res.push(new_url);
+                      }
+
+                      distribution.urls_queue.store.del(encodedURL, (e, v) => {
+                        if (e) console.log(e);
+                        let newURLsCounter = 0;
+                        let totalNewUrls = res.length
+                        if (newURLsCounter == totalNewUrls) {
+                          return cb(res);
+                        }
+                        res.forEach((u) => {
+                          let encodedURL = encodeURIComponent(u);
+                          distribution.page_content.store.keyExists(encodedURL, (e, exists) => {
+                            if (exists) {
+                              newURLsCounter++;
+                              if (newURLsCounter == totalNewUrls) {
+                                res = res.map((u) => {return {[encodeURIComponent(u)]: 1}; });
+                                return cb(res);
+                              } 
+                            } else {
+                              distribution.urls_queue.store.put(u, encodeURIComponent(u), (e, v) => {
+                                if (e) console.log(e);
+                                newURLsCounter++;
+                                if (newURLsCounter == totalNewUrls) {
+                                  res = res.map((u) => {return {[encodeURIComponent(u)]: 1}; });
+                                  return cb(res);
+                                }
+                              })
+                            }
+                          })
+                        })
+                      })
+                    })
+
+                  }
                 })
-              })
+              }
+
             })
+
           }
 
           const reducer = (encodedURL, values) => {
@@ -156,14 +199,14 @@ distribution.local.groups.put({gid: "urls_queue", hash: id.naiveHash}, groupA, (
           }
 
           distribution.urls_queue.store.get(null, (e, v) => {
-            console.log(v);
+            // console.log(v);
             if (e) console.log(e);
             const start = performance.now();
             distribution.urls_queue.mr.exec({keys: v, map: mapper, reduce: reducer}, (e, v) => {
               const total_time = performance.now() - start;
-              console.log("v:", v);
+              // console.log("v:", v);
               const values = Object.keys(v[0]).map(url => decodeURIComponent(url));
-              console.log(`Total time for ${values.length} matches: ${total_time}`);
+              // console.log(`Total time for ${values.length} matches: ${total_time}`);
             })
           })
 
